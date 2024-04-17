40a02592e17661fd01bb0553246ea2fd416d5997
xoetek <mnisioo03@gmail.com>
2024-04-17 20:24:23 +0200

    this is so peak

--- shaders/vShader.vs
+++ shaders/vShader.vs
@@ -1,10 +1,16 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
-uniform float xOffset;
+layout (location = 2) in vec2 aTexCoord;
+
 out vec3 ourColor;
+out vec2 TexCoord;
+
+uniform float xOffset;
+
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y + xOffset, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x + xOffset, aPos.y, aPos.z, 1.0);
     ourColor = aColor;
+    TexCoord = aTexCoord;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <shader.h>
 #include <iostream>
+#include <stb_image.h>
 
 // Vertex Shader
 int getRefreshRate(GLFWmonitor* monitor)
@@ -69,55 +70,115 @@ int main()
         return -1;
     }
 
-
-
-    // Define vertices of the triangle
     float vertices[] = {
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+        //pos                //color            //texture
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f
     };
-
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
     // Create vertex buffer object (VBO) and vertex array object (VAO)
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // Bind VAO and VBO
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     Shader shader("D:/Goodies/C++/learning-stuff/shaders/vShader.vs", "D:/Goodies/C++/learning-stuff/shaders/fShader.fs");
     
-    // Enable depth testing
-    glEnable(GL_DEPTH_TEST);
+    int width, height, nrChannels;
+    unsigned int texture1, texture2;
+    stbi_set_flip_vertically_on_load(true);  
+    unsigned char *data = stbi_load("D:/Goodies/C++/learning-stuff/textures/nah_id_win.jpg", &width, &height, &nrChannels, 0);
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    if (data == NULL)
+    {
+        std::cout << "Failed to load texture" << std::endl;
+        return -1;
+    }
+    else
+    {
+        std::cout << "Loaded texture" << std::endl;
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+
+    data = stbi_load("D:/Goodies/C++/learning-stuff/textures/;3.jpg", &width, &height, &nrChannels, 0);
+    if (data == NULL)
+    {
+        std::cout << "Failed to load texture" << std::endl;
+        return -1;
+    }
+    else
+    {
+        std::cout << "Loaded texture" << std::endl;
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
     // Render loop
-    float val = 0.0f;
     while (!glfwWindowShouldClose(window))
     {
-        
-        val = (cos(glfwGetTime())/2.0);
-        shader.use();
-        shader.setFloat("xOffset", val);
-        // Process user input
         processInput(window, monitor);
         glfwSetKeyCallback(window, key_callback);
+
+        double val = (sin(glfwGetTime())/2.0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        
+        shader.use();
+        shader.setFloat("xOffset", val);
+        shader.setFloat("mixValue", val);
+        // Process user input
+        
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Use shader program
-
         
         // Bind VAO
-        glBindVertexArray(VAO);
+        glBindVertexArray(VBO);
 
         // Draw the triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Unbind VAO
         glBindVertexArray(0);
